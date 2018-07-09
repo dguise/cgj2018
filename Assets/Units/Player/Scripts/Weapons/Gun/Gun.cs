1c96f6a3ab66903d30fc393df9bf7f12c1350a62
Magnus Tullberg <flummeltrollet@gmail.com>
2018-07-09 19:34:59 +0200

    add cooldown to Gun and a speed variable

--- Assets/Units/Player/Scripts/Weapons/Gun/Gun.cs
+++ Assets/Units/Player/Scripts/Weapons/Gun/Gun.cs
@@ -3,14 +3,23 @@ using UnityEngine;
 class Gun : IWeapon {
 	public GameObject attackWeapon;
     GameObject spawnAttack;
+	float cooldown = 0.2f;
+	float speed = 4f;
+	float lastAttack;
 
     public Gun()
 	{
 		attackWeapon = Resources.Load<GameObject>("Bullet");
+		lastAttack = -(cooldown + 1) ;
 	}
 
 	public void Attack(Vector2 position, Vector2 direction, Quaternion rotation, float radius) {
-		spawnAttack = MonoBehaviour.Instantiate(attackWeapon, position + direction * radius, rotation);
-		spawnAttack.GetComponent<Rigidbody2D>().velocity = direction;
+		var currentTime = Time.time;
+		if (lastAttack + cooldown < currentTime) 
+		{
+			lastAttack = currentTime;
+			spawnAttack = MonoBehaviour.Instantiate(attackWeapon, position + direction * radius, rotation);
+			spawnAttack.GetComponent<Rigidbody2D>().velocity = direction * speed;
+		}
     }
 }
