c3c78182a5640ae73f1059b0f066f3f5561c4b3c
Per <perjonaserik@gmail.com>
2018-07-09 17:39:05 +0200

    Startmenu gui + playermanager

---
+++ Assets/Helpers/PlayerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerManager {
+
+	public enum CharacterClassesEnum
+    {
+        Emil,
+        Melee,
+        Bowman,
+        Magician,
+        Roger,
+        Dartblower
+    }
+
+    public static CharacterClassesEnum playerOneClass = CharacterClassesEnum.Emil;
+    public static CharacterClassesEnum playerTwoClass = CharacterClassesEnum.Emil;
+
+    public static bool playerOneReady = false;
+    public static bool playerTwoReady = false;
+
+
+}
