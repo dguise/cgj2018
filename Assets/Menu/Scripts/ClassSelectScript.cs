3a9265e7d5556f36e0707cea006f2ed6fc905e38
Per <perjonaserik@gmail.com>
2018-07-09 20:00:25 +0200

    Class Selector och headless axeman,arbetsolycka?

---
+++ Assets/Menu/Scripts/ClassSelectScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class ClassSelectScript : MonoBehaviour {
+
+    public Sprite[] portraitsArray = new Sprite[Enum.GetNames(typeof(PlayerManager.CharacterClassesEnum)).Length];
+    private int choices = Enum.GetNames(typeof(PlayerManager.CharacterClassesEnum)).Length;
+    private int currentChoice = 0;
+    private UnityEngine.UI.Image portraitImage;
+
+    void Start ()
+    {
+        this.portraitImage = this.GetComponentsInChildren<UnityEngine.UI.Image>()[3];
+    }
+	
+	void Update ()
+    {
+        if (Input.anyKey)
+        {
+            this.currentChoice += 1;
+            if (currentChoice == choices)
+            {
+                this.currentChoice = 0;
+            }
+            this.portraitImage.sprite = portraitsArray[this.currentChoice];
+        }
+    }
+}
