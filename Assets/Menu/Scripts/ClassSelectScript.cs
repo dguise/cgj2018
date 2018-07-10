50f6af51780b4f3ea1946377eecc53113e217406
Magnus Tullberg <flummeltrollet@gmail.com>
2018-07-10 23:56:27 +0200

    Update class selection, now id based.

--- Assets/Menu/Scripts/ClassSelectScript.cs
+++ Assets/Menu/Scripts/ClassSelectScript.cs
@@ -10,27 +10,35 @@ public class ClassSelectScript : MonoBehaviour {
     private int choices = Enum.GetNames(typeof(PlayerManager.CharacterClassesEnum)).Length;
     private int currentChoice = 0;
     private UnityEngine.UI.Image portraitImage;
+    public int playerID = 1;
+    public float timestamp;
+    float cooldown = 0.3f;
 
     void Start ()
     {
         this.portraitImage = this.GetComponentsInChildren<UnityEngine.UI.Image>()[3];
+        timestamp = -cooldown;
     }
 	
-	void Update ()
+	void FixedUpdate() 
     {
-        if (Input.anyKeyDown)
+		float horizontal = Input.GetAxisRaw(Inputs.Horizontal(playerID));
+        if (Mathf.Abs(horizontal) > 0.5 && timestamp + cooldown < Time.time)
         {
-            this.currentChoice += 1;
-            if (currentChoice == choices)
-            {
-                this.currentChoice = 0;
-            }
+            timestamp = Time.time;
+            this.currentChoice = ((this.currentChoice + (int)Mathf.Ceil(horizontal)) % choices + choices) % choices;
 
             this.portraitImage.sprite = portraitsArray[this.currentChoice];
-            Transform rightArrow = transform.Find("RightArrow");
             
-            var anim = rightArrow.GetComponent<Animator>();
-            anim.SetTrigger("RightArrow");
+            if (horizontal > 0) {
+                Transform rightArrow = transform.Find("RightArrow");
+                var anim = rightArrow.GetComponent<Animator>();
+                anim.SetTrigger("RightArrow");
+            } else {
+                Transform leftArrow = transform.Find("LeftArrow");
+                var anim = leftArrow.GetComponent<Animator>();
+                anim.SetTrigger("LeftArrow");
+            }
         }
     }
 }
