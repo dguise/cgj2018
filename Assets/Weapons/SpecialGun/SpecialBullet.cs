4c492f2f5b65c09fdb87dde77081fc4c20598ac6
Per <perjonaserik@gmail.com>
2018-07-11 10:47:05 +0200

    Här om ni vill slippa debug.log

--- Assets/Weapons/SpecialGun/SpecialBullet.cs
+++ Assets/Weapons/SpecialGun/SpecialBullet.cs
@@ -30,7 +30,7 @@ public class SpecialBullet : Projectile
         life += Time.deltaTime * bulletSpeed;
         var rbody = this.GetComponent<Rigidbody2D>();
         Vector2 vectorFromGraph =  new Vector2(Time.deltaTime * bulletSpeed, (bulletCurve.Evaluate((life + Time.deltaTime * bulletSpeed) / lifetime) - bulletCurve.Evaluate(life / lifetime)));
-        Debug.Log(Vector2.SignedAngle(Vector2.right, direction));
+        //Debug.Log(Vector2.SignedAngle(Vector2.right, direction));
         vectorFromGraph = vectorFromGraph.MaakepRotate(Vector2.SignedAngle(Vector2.right, direction));
         rbody.velocity = vectorFromGraph * bulletRange * 100f;
     }
