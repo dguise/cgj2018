24067506f1ed6cf9b2756747ded3dc60612b8fda
Magnus Tullberg <flummeltrollet@gmail.com>
2018-07-11 16:01:06 +0200

    add input from all joysticks support

--- ProjectSettings/InputCreator.sh
+++ ProjectSettings/InputCreator.sh
@@ -6,4 +6,10 @@ do
     cat InputTemplate.temp >> InputTemplateBase.temp
     rm InputTemplate.temp
 done   
+
+cp InputTemplate InputTemplate.temp
+sed -i -- "s/joyNum: TEMP/joyNum: 0/g" InputTemplate.temp
+sed -i -- "s/TEMP//g" InputTemplate.temp
+cat InputTemplate.temp >> InputTemplateBase.temp
+rm InputTemplate.temp
 mv InputTemplateBase.temp InputManager.asset
