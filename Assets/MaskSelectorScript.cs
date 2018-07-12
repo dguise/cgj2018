2cfb73182d0a0d09d4e38bdb0007868feb0f8f20
Per <perjonaserik@gmail.com>
2018-07-12 22:47:16 +0200

    Maskat en massa. Switch på character. WOW

--- Assets/MaskSelectorScript.cs
+++ Assets/MaskSelectorScript.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MaskSelectorScript : MonoBehaviour {
+public class MaskSelectorScript : MonoBehaviour
+{
 
     public List<Mesh> listOfMeshes = new List<Mesh>(Enum.GetNames(typeof(PlayerManager.CharacterClassesEnum)).Length);
     public List<MeshFilter> listOfMeshFilters = new List<MeshFilter>(Enum.GetNames(typeof(PlayerManager.CharacterClassesEnum)).Length);
@@ -11,13 +12,19 @@ public class MaskSelectorScript : MonoBehaviour {
     Mesh currentMesh;
     MeshFilter currentMeshFilter;
 
-    void Start () {
+    void Start()
+    {
         currentMesh = GetComponent<Mesh>();
         currentMeshFilter = GetComponent<MeshFilter>();
     }
-	
+
     public void SetMask(PlayerManager.CharacterClassesEnum selectedClass)
     {
+        foreach (Transform kiddo in transform)
+        {
+            kiddo.gameObject.SetActive(false);
+
+        }
         transform.GetChild((int)selectedClass).gameObject.SetActive(true);
 
         //currentMesh = listOfMeshes[(int)selectedClass];
