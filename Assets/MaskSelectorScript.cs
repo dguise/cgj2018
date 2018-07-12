15cfb1c1c29cc64150442b3681957d8288ba4db5
Per <perjonaserik@gmail.com>
2018-07-12 21:28:56 +0200

    idk. 2nd stuff

---
+++ Assets/MaskSelectorScript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskSelectorScript : MonoBehaviour {
+
+    public List<Mesh> listOfMeshes = new List<Mesh>(Enum.GetNames(typeof(PlayerManager.CharacterClassesEnum)).Length);
+    public List<MeshFilter> listOfMeshFilters = new List<MeshFilter>(Enum.GetNames(typeof(PlayerManager.CharacterClassesEnum)).Length);
+
+    Mesh currentMesh;
+    MeshFilter currentMeshFilter;
+
+    void Start () {
+        currentMesh = GetComponent<Mesh>();
+        currentMeshFilter = GetComponent<MeshFilter>();
+    }
+	
+    public void SetMash(PlayerManager.CharacterClassesEnum selectedClass)
+    {
+        currentMesh = listOfMeshes[(int)selectedClass];
+        //currentMeshFilter = listOfMeshFilters[(int)selectedClass];
+    }
+}
