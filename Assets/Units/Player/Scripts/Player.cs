b281bec16b36c167f2f47087cbda92388a73932e
Per <perjonaserik@gmail.com>
2018-07-12 12:20:27 +0200

    Fab Swag Dash!

--- Assets/Units/Player/Scripts/Player.cs
+++ Assets/Units/Player/Scripts/Player.cs
@@ -3,32 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent (typeof (Rigidbody2D))]
-public class Player : Unit {
-	public Weapon weapon;
-	float radius = 0.5f;
-	const float DEADZONE = 0.98f;
-	public int playerID;
-	Animator anim;
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : Unit
+{
+    public Weapon weapon;
+    float radius = 0.5f;
+    const float DEADZONE = 0.98f;
+    public int playerID;
+    Animator anim;
 
     Transform head;
     Transform body;
 
     Rigidbody2D rb;
 
+    private bool canDash = true;
+    private bool dashing = false;
+    private float dashTime = 0.5f;
+    private float dashResetTime
+    {
+        get { return dashTime * 10; }
+    }
+
     public PlayerManager.CharacterClassesEnum PlayerClass;
 
-	void Start () {
+    void Start()
+    {
         weapon = PlayerManager.GetWeapon(PlayerClass, gameObject);
 
         head = transform.Find("monkeyhead");
         body = transform.Find("Armature.001");
 
-		anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
 
         DontDestroyOnLoad(this);
-	}
+    }
 
 
     private void Update()
@@ -43,30 +53,68 @@ public class Player : Unit {
             weapon = new EnemySpawnerGun(gameObject);
     }
 
-    private void FixedUpdate() {
-        if (PlayerManager.playerReady[playerID]){
-            Vector2 playerVelocity = new Vector2(Input.GetAxisRaw(Inputs.Horizontal(PlayerManager.controllerId[playerID])), Input.GetAxisRaw(Inputs.Vertical(PlayerManager.controllerId[playerID])));
-            rb.velocity = playerVelocity.normalized * movementSpeed;
-            anim.SetFloat(AnimatorConstants.Speed, playerVelocity.magnitude);
-
-            if (rb.velocity.magnitude > DEADZONE)
+    private void FixedUpdate()
+    {
+        if (PlayerManager.playerReady[playerID])
+        {
+            //Movement
+            if (Input.GetButtonDown(Inputs.AButton(playerID + 1)))
             {
-                body.eulerAngles = new Vector3(body.eulerAngles.x, body.eulerAngles.y, (Mathf.Atan2(rb.velocity.x, rb.velocity.y) * 180 / Mathf.PI) + 180);
+                if (canDash)
+                    StartCoroutine(Dash());
+            }
+            else if (!dashing)
+            {
+                Vector2 playerVelocity = new Vector2(Input.GetAxisRaw(Inputs.Horizontal(PlayerManager.controllerId[playerID])), Input.GetAxisRaw(Inputs.Vertical(PlayerManager.controllerId[playerID])));
+                rb.velocity = playerVelocity.normalized * movementSpeed;
+                anim.SetFloat(AnimatorConstants.Speed, playerVelocity.magnitude);
+
+                if (rb.velocity.magnitude > DEADZONE)
+                {
+                    body.eulerAngles = new Vector3(body.eulerAngles.x, body.eulerAngles.y, (Mathf.Atan2(rb.velocity.x, rb.velocity.y) * 180 / Mathf.PI) + 180);
+                }
             }
 
+            //Attack
             Vector2 attackDirection = new Vector2(Input.GetAxisRaw(Inputs.FireHorizontal(PlayerManager.controllerId[playerID])), Input.GetAxisRaw(Inputs.FireVertical(PlayerManager.controllerId[playerID])));
 
             Vector2 attackPosition = transform.position;
             Quaternion attackRotation = Quaternion.identity;
-            if (attackDirection.magnitude > DEADZONE) {
+            if (attackDirection.magnitude > DEADZONE)
+            {
                 weapon.Attack(attackPosition, attackDirection, attackRotation, radius);
                 head.eulerAngles = new Vector3(head.eulerAngles.x, head.eulerAngles.y, (Mathf.Atan2(attackDirection.y, attackDirection.x) * 180 / Mathf.PI) * -1 - 90);
             }
-        } else {
-            if (!PlayerManager.playerReady[playerID]) {
+        }
+        else
+        {
+            if (!PlayerManager.playerReady[playerID])
+            {
                 PlayerManager.MapControllerToPlayer();
             }
         }
+    }
+
+    IEnumerator Dash()
+    {
+        dashing = true;
+
+        Vector2 lastDirection = rb.velocity.normalized;
+
+        rb.velocity = Vector2.zero;
+
+
+        rb.AddRelativeForce(-1 * (lastDirection * movementSpeed * 2), ForceMode2D.Impulse);
+        anim.SetFloat(AnimatorConstants.Speed, rb.velocity.magnitude);
+
+        yield return new WaitForSeconds(dashTime);
+        Invoke("EnableDash", dashResetTime);
+        dashing = false;
+    }
+
+    void EnableDash()
+    {
+        canDash = true;
     }
 
     public override void TakeDamageExtender(float damage, GameObject sender, Collider2D collider)
@@ -74,4 +122,3 @@ public class Player : Unit {
         // Currently don't give af
     }
 }
-    
