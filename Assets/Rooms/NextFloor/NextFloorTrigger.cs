7a8099844818204a27c2000513994bede17f8854
Maakep <schon.fredrik@gmail.com>
2018-07-13 18:58:28 +0200

    Next floor object

---
+++ Assets/Rooms/NextFloor/NextFloorTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextFloorTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == Tags.Player)
+        {
+            LevelManager.TempleFloor++;
+            if (LevelManager.TempleFloor == 4)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                return;
+            }
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
