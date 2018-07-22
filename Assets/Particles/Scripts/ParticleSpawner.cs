a6f626601520e7f402b7722f8db804793032ae05
Maakep <schon.fredrik@gmail.com>
2018-07-22 15:12:13 +0200

    Add enums corresponding to prefab names

--- Assets/Particles/Scripts/ParticleSpawner.cs
+++ Assets/Particles/Scripts/ParticleSpawner.cs
@@ -1,29 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+
+public enum ParticleTypes
+{
+    YellowPixelExplosion,
+    BloodParticles,
+    LevelUp,
+    SiphonBloodAbility,
+    BlueGlitter_OverDistance,
+    BlueGlitter_OverTime,
+    BluePixelTrail,
+    BluePixelTrail_Fewer,
+    BluePixelTrail_Streched_Fast,
+    Fire,
+    Footsteps,
+    RedPixelExplosion_Up,
+    StaticFire,
+}
 
 public class ParticleSpawner : MonoBehaviour
 {
     public static ParticleSpawner instance = null;
 
-    public enum ParticleTypes
-    {
-        YellowPixelExplosion,
-        BloodParticles,
-        LevelUp,
-        SiphonBloodAbility,
-        BlueGlitter_OverDistance,
-        BlueGlitter_OverTime,
-        BluePixelTrail,
-        BluePixelTrail_Fewer,
-        BluePixelTrail_Streched_Fast,
-        Fire,
-        Footsteps,
-        RedPixelExplosion_Up,
-        StaticFire,
-    }
 
     // No usage, just a quick reference in the Inspector to see what order the particles should be entered at
+    // Would be nice if this was a serialized dictionary.
     [SerializeField]
     private ParticleTypes _listIndexReference;
     public List<GameObject> particleEffects;
